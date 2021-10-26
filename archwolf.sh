25f49601ba54e7e95c08240587c40d123b3b6efb
wolfee182 <30846933+wolfee182@users.noreply.github.com>
2021-10-26 15:41:43 -0700

    Update archwolf.sh

--- archwolf.sh
+++ archwolf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
-    source /mnt/root/ArchTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    arch-chroot /mnt /root/ArchWolf/1-setup.sh
+    source /mnt/root/ArchWolf/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchWolf/2-user.sh
+    arch-chroot /mnt /root/ArchWolf/3-post-setup.sh
